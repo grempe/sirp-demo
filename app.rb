a5863bbfae34eb703008e4e2dc3925bc613f9b3b
Glenn Rempe <glenn@rempe.us>
2016-05-16 16:37:02 -0700

    Use Ohm + Redis to store and retrieve all user data.

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@
 # require 'bundler/setup'
 require 'sinatra'
 require 'json'
+require 'hashie'
+require 'ohm'
+require 'ohm/contrib'
 require 'sirp'
 require 'logger'
 logger = Logger.new $stdout
+
+include Hashie::Extensions::SymbolizeKeys
 
 # Sinatra : Use Puma
 configure { set :server, :puma }
@@ -17,52 +22,80 @@ set :public_folder, 'public'
 # Set prime N length - client has to use the same value!
 prime_length = 4096
 
-# Simulated DB
-users = {
-  leonardo: 'capricciosa',
-  raphael: 'quattro formaggi',
-  donatello: 'margherita',
-  michelangelo: 'tropicana'
-}
-
-user_verifiers = users.map do |username, password|
-  { username => SIRP::Verifier.new(prime_length).generate_userauth(username.to_s, password) }
+if ENV['REDIS_URL']
+  Ohm.redis = Redic.new(ENV['REDIS_URL'])
 end
 
-user_verifiers.each { |h| users.update h }
+class User < Ohm::Model
+  include Ohm::DataTypes
+
+  attribute :username
+  attribute :salt
+  attribute :verifier
+  attribute :proof, Type::Hash
+  unique :username
+  index :username
+end
+
+# User.create(SIRP::Verifier.new(4096).generate_userauth('leonardo', 'capricciosa')).save
 
 before do
   content_type 'application/json'
-  # response['Access-Control-Allow-Origin'] = '*'
 end
 
 get '/' do
   redirect '/index.html'
 end
 
-post '/authenticate' do
-  username = params[:username]
-  user = users[username.to_sym]
+post '/users' do
+  if params[:username] && params[:username] =~ /^[a-zA-Z\.\@]+/ && params[:salt] && params[:verifier]
+    if User.find(username: params[:username]).first
+      logger.warn "Username #{params[:username]} already exists"
+      halt 409, 'user_conflict'
+    end
 
-  unless user
-    logger.warn "User #{username} not found"
+    User.create(username: params[:username], salt: params[:salt], verifier: params[:verifier]).save
+    halt 201, 'user_created'
+  else
+    halt 400, 'user_missing_params'
+  end
+end
+
+get '/users/:username' do
+  if params[:username] && params[:username] =~ /^[a-zA-Z\.\@]+/
+    user = User.find(username: params[:username]).first
+    return JSON.generate(user.attributes) if user
+    halt 404
+  else
+    halt 400, 'user_missing_params'
+  end
+end
+
+post '/authenticate' do
+  if params[:username] && params[:username] =~ /^[a-zA-Z\.\@]+/
+    user = User.find(username: params[:username]).first
+
+    unless user && user.username && user.salt && user.verifier
+      logger.warn "User #{params[:username]} not found"
+      halt 401
+    end
+  else
+    logger.warn 'Username must be present and valid'
     halt 401
   end
 
   if params[:A]
     logger.info 'P1 : Starting'
-    logger.info "P1 : Server received username '#{username}' and A"
+    logger.info "P1 : Server received username '#{user.username}' and A"
     logger.info "P1 : Client A : #{params[:A]}"
-    aa = params[:A]
-    v = user[:verifier]
-    salt = user[:salt]
 
     # Server generates B, saves A and B to database
     verifier = SIRP::Verifier.new(prime_length)
-    session = verifier.get_challenge_and_proof(username, v, salt, aa)
+    session = verifier.get_challenge_and_proof(user.username, user.verifier, user.salt, params[:A])
 
     logger.info 'P1 : Server persisting user verifier (proof)'
-    user[:session_proof] = session[:proof]
+    user.proof = session[:proof]
+    user.save
 
     logger.info 'P1 : Server sending salt and B'
     logger.info "P1 : Server salt : #{session[:challenge][:salt].length} : #{session[:challenge][:salt]}"
@@ -70,12 +103,12 @@ post '/authenticate' do
     return JSON.generate(session[:challenge])
   elsif params[:M]
     logger.info 'P2 : Starting'
-    logger.info "P2 : Server received username '#{username}' and client M"
+    logger.info "P2 : Server received username '#{user.username}' and client M"
     client_M = params[:M]
     logger.info "P2 : Client M : #{client_M.length} : #{client_M}"
 
-    logger.info 'P2 : Retrieving verifier from the database'
-    proof = user[:session_proof]
+    logger.info 'P2 : Retrieving proof from the database'
+    proof = Hashie.symbolize_keys(user.proof)
 
     logger.info 'P2 : Verifying client/server M match, generating H_AMK'
     verifier = SIRP::Verifier.new(prime_length)
@@ -83,7 +116,7 @@ post '/authenticate' do
     logger.info "P2 : server M: #{verifier.M}"
 
     if server_H_AMK
-      logger.info "P2 : #{username} Authenticated!"
+      logger.info "P2 : #{user.username} Authenticated!"
       logger.info "P2 : Client and server negotiated shared key K : #{verifier.K}"
       logger.info "P2 : Server sending final H_AMK : #{server_H_AMK.length} : #{server_H_AMK}"
       return JSON.generate(H_AMK: server_H_AMK)
--- config.ru
+++ config.ru
@@ -2,6 +2,9 @@
 # ^^^ run puma by default when 'bundle exec rackup'
 # or
 # bundle exec puma -t 8:32 -w 2 --preload --control tcp://127.0.0.1:9293 --control-token myapp
+#
+# Access admin with commands [stop, halt, restart]:
+# http://localhost:9293/restart?token=myapp
 
 require 'rubygems'
 require 'bundler'
